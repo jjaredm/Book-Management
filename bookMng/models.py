59ca01121ff46d4581a34e379a2436f62709b4ca
jjaredm <jared.martinez50@yahoo.com>
2025-04-18 19:29:23 -0700

    Initial Commit

---
+++ bookMng/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+#import something check notes for 2/19
+
+# Create your models here.
+
+
+class MainMenu(models.Model):
+    item = models.CharField(max_length=300, unique=True)
+    link = models.CharField(max_length=300, unique=True)
+
+    def __str__(self):
+        return self.item
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    web = models.URLField(max_length=300)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    publishdate = models.DateField(auto_now=True)
+    picture = models.FileField(upload_to='bookEx/static/uploads')
+    pic_path = models.CharField(max_length=300)
+    username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+
