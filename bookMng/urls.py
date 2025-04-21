49169e207d092bd3f5a2d5a1093bd40d7ab28a26
jjaredm <jared.martinez50@yahoo.com>
2025-04-21 16:18:44 -0700

    added search url

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('postbook', views.postbook, name='postbook'),
     path('displaybooks', views.displaybooks, name='displaybooks'),
     path('mybooks', views.mybooks, name='mybooks'),
+    path('search/', views.search_book, name='search_book'),
     path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
     path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
 ]
